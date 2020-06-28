3e974883a1584e57aa755c827ff066e123f054de
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-28 16:22:25 +0530

    Update Game.js

--- Js/Game.js
+++ Js/Game.js
@@ -4,6 +4,7 @@ class Game {
   }
 
   getState(){
+    // Give the information in database
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
        gameState = data.val();
@@ -16,7 +17,8 @@ class Game {
       gameState: state
     });
   }
-
+// make start function 
+  // it will wait for all players to login 
   async start(){
     if(gameState === 0){
       player = new Player();
@@ -28,7 +30,8 @@ class Game {
       form = new Form()
       form.display();
     }
-
+// Make sprites of Cars (car1 , car2 , car3)
+    // add the image in sprites
     car1 = createSprite(100,100,60,60);
     car1.addImage("c", c1Img);
     car2 = createSprite(200,100,60,60);
@@ -37,13 +40,23 @@ class Game {
     car3.addImage("c", c3Img);
     cars = [car1, car2, car3];
   }
-
+// make play function 
   play(){
+    // Hide the form 
     form.hide();
+    
+    // Give informations of players
     Player.getPlayerInfo();
+    
+    // give the informations of cars to the player
     player.getCarsAtEnd();
+    
     if(allPlayers !== undefined){
-      background(rgb(198,135,103));
+      
+      // Give color to the background
+      background(rgb(200,135,103));
+      
+      // Give image to the background 
       background(track , 0,-displayWidth*4,displayHeight, displayWidth*5 );
 
       //index of the array
@@ -63,17 +76,20 @@ class Game {
         //position the cars a little away from each other in x direction
         x = allPlayers[plr].distance;        
         cars[index-1].x = x;
-
+        
+        // Create the sprities for hurdles 
         hurdle1 = createSprite(300, y, 50, 50);
         hurdle1.shapeColor = "white";
 
         hurdle2 = createSprite(800, y, 50, 50);
         hurdle2.shapeColor = "white";
  
-        
-        if(keyIsDown(UP_ARROW)){    
+        // By this statement car will go up side
+        if(keyIsDown(UP_ARROW)){  
           cars[index-1].y-=60;
         }
+        // Collide the car with hurdles 
+        // When car collide the hurdles it will go to its original position (starting point)
         if(cars[index-1].collide(hurdle1) || cars[index-1].collide(hurdle2)){    
           if(cars[index-1].y%150===0){    
             player.distance = 0;
@@ -81,7 +97,9 @@ class Game {
           }
         }
 
-        if (index === player.index){     
+        // it will help the player to recognise that which car will belongs to him
+        // it will show a red circle at the car position
+        if (index === player.index){   
         stroke(10);
         fill("red");
         ellipse(x,y,80,80);     
@@ -91,15 +109,17 @@ class Game {
         }
       }
     }
-
+// by this statement it will move right side
     if(keyIsDown(RIGHT_ARROW)&& player.index !== null){
       Player.getPlayerInfo();
       console.log(player.distance);
       player.distance +=10
       player.update();
     }
-
+// it will give the position and rank to the database
+    // it will show you your rank and game state ended
     if(player.distance >= 1270){
+      // Player rank will increaes
       player.rank = player.rank+1;
     Player.updateCarsAtEnd(player.rank);
       textSize(24);
